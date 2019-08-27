08ccbd3cc16d9c7c861b66add399cf6b944b36b0
babrodtk <andre.r.brodtkorb@gmail.com>
2019-08-27 08:45:24 +0200

    Committing initial version

---
+++ src/no/oslomet/cs/algdat/Week1.java
@@ -0,0 +1,47 @@
+package no.oslomet.cs.algdat;
+
+import java.util.Random;
+
+public class Week1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        int num_values = 10;
+        int[] values = new int[num_values];
+
+        long tic = System.currentTimeMillis();
+        Random rand = new Random();
+        for (int i=0; i<num_values; ++i) {
+            values[i] = rand.nextInt(100);
+        }
+        long toc = System.currentTimeMillis();
+
+        System.out.println("Elapsed: " + (toc-tic) + " ms");
+
+
+    }
+
+    public static int findMax(int[] values) {
+        int max_value = 0;
+
+        for (int i=0; i<values.length; ++i) {
+            if (max_value < values[i]) {
+                max_value = values[i];
+            }
+        }
+        return max_value;
+    }
+
+    public static String toString(int[] values) {
+        String retval = "";
+
+        retval = "[" + values[0];
+        for (int i=1; i<values.length; ++i) {
+            retval = retval + ", " + values[i];
+        }
+        retval = retval + "]";
+
+        return retval;
+    }
+}
---
+++ src/no/oslomet/cs/algdat/Week1Test.java
@@ -0,0 +1,26 @@
+package no.oslomet.cs.algdat;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Week1Test {
+    @org.junit.jupiter.api.Test
+    void findMax() {
+        int[] values = {1, 2, 3, 4, 5};
+
+        assertEquals(Week1.findMax(values), 5);
+    }
+
+    @org.junit.jupiter.api.Test
+    void toStringTest() {
+        int[] values = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(values));
+        System.out.println(Week1.toString(values));
+
+        assertEquals(Week1.toString(values), Arrays.toString(values));
+    }
+
+
+}
