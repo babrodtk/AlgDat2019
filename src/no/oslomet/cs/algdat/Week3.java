1a7db8ad26951702d6059a01e5dac0de3e03daab
babrodtk <andre.r.brodtkorb@gmail.com>
2019-09-03 11:14:56 +0200

    Cleaned up code a bit and added documentation

--- src/no/oslomet/cs/algdat/Week3.java
+++ src/no/oslomet/cs/algdat/Week3.java
@@ -4,60 +4,113 @@ import java.util.Arrays;
 
 public class Week3 {
 
+    /**
+     * Function which finds the index of the maximum value
+     * in the interval [left, right]
+     * @param values Values to search through
+     * @param left
+     * @param right
+     * @return Index to maximum value
+     */
     public static int findMax(int[] values, int left, int right) {
-        //FIXME: vi må sjekke argumentene her!
+
+        // FIXME: We need to check arguments here!
+
+        // Initialize max value and index to first element
         int max_value = values[left];
         int max_index = left;
+
+        // Loop through all remaining elements and find max
         for (int i=left+1; i<=right; ++i) {
             if (values[i] > max_value) {
                 max_value = values[i];
                 max_index = i;
             }
         }
+
+        //Return max index
         return max_index;
     }
 
     /**
+     * Swaps the two elements values[from] and values[to]
      * @param values
+     * @param from
+     * @param to
+     */
+    public static void swap(int[] values, int from, int to) {
+        int tmp = values[from];
+        values[from] = values[to];
+        values[to] = tmp;
+    }
+
+    /**
+     * Sorts an array using selection sort,
+     * @see https://www.cs.hioa.no/~ulfu/appolonius/kap1/3/kap13.html#1.3.4
+     * @param values Values to sort (in place)
      */
     public static void selectionSort(int[] values) {
+
+        //Sort the array one element at a time
         for (int i=0; i<values.length-1; ++i) {
-            int max_index = findMax(values, i, values.length -1);
-            System.out.println(i + " - " + (values.length-1));
-            System.out.println(values[max_index]);
+            System.out.println("Iteration " + i);
+            System.out.println("Before sort: " + Arrays.toString(values));
 
-            //Bytt elementer
-            int temp = values[max_index];
-            values[max_index] = values[i];
-            values[i] = temp;
+            //Determine the interval to find the maximum value
+            int left = i;
+            int right = values.length-1;
 
-            System.out.println(Arrays.toString(values));
+            System.out.println("Searching in [" + left + ", " + right + "]");
+
+            //Find index of maximum value
+            int max_index = findMax(values, left, right);
+
+            System.out.println("Maximum is " + values[max_index] + " at position " + max_index);
+
+            //Swap elements - move maximum to left
+            swap(values, left, max_index);
+
+            //Print array
+            System.out.println("After sort: " + Arrays.toString(values));
         }
-
     }
 
 
+    /**
+     * Uses binary search to find a value in an array
+     * @param values Values to search through
+     * @param value Value to search for
+     * @return Index of value if found, or where the value would be
+     */
     public static int binarySearch(int[] values, int value) {
         int left = 0;
         int right = values.length-1;
 
+        /**
+         * Search while we have an interval to search through
+         */
         while (left <= right) {
-            System.out.println("Søker i " + left + " - " + right);
-            //NOTE: heltallsdivisjon
+            System.out.println("Searching in [" + left + ", " + right + "]");
+
+            //NOTE: Integer division!
             int mid = (left + right) / 2;
 
             if (value < values[mid]) {
+                //Search left of mid
                 right = mid - 1;
             }
             else if (value > values[mid]) {
+                //Search right of mid
                 left = mid + 1;
             }
             if (value == values[mid]) {
+                //Found value
                 return mid;
             }
-
         }
 
+        //Value not found!
+        //Returning where it should be placed
         return left;
     }
 
--- src/no/oslomet/cs/algdat/Week3Test.java
+++ src/no/oslomet/cs/algdat/Week3Test.java
@@ -12,35 +12,40 @@ class Week3Test {
     void findMax() {
         int[] values = {1, 7, 2, 4, 6};
 
-        assertEquals(1, Week3.findMax(values, 0, 4));
-
+        //Search in [0, 4] - check index
         int max_1 = Week3.findMax(values, 0, 4);
+        assertEquals(1, max_1);
         assertEquals(7, values[max_1]);
 
+        //Search in [2, 4]
         int max_2 = Week3.findMax(values, 2, 4);
         assertEquals(4, max_2);
         assertEquals(6, values[max_2]);
+
+        //FIXME: Check more arrays!
     }
 
     @Test
     void selectionSort() {
         int[] values = {1, 7, 2, 4, 6};
+        int[] sorted = {7, 6, 4, 2, 1};
 
+        //NOTE: sorts in place (changes values)
+        //NOTE: Uses assertArrayEquals to check arrays for equality
         Week3.selectionSort(values);
-
-        System.out.println(Arrays.toString(values));
+        assertArrayEquals(values, sorted);
     }
 
     @Test
     void binarySearch() {
         int[] values = {1, 2, 4, 6, 7, 9, 17};
 
+        //Search for 7
         int index_7 = Week3.binarySearch(values, 7);
         assertEquals(4, index_7);
 
+        //Search for 8 (not found, should be in place 5)
         int index_8 = Week3.binarySearch(values, 8);
         assertEquals(5, index_8);
-
-
     }
 }
