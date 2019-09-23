1a13dafeaedbb4f8c62fb77ad1c21519198d8132
babrodtk <andre.r.brodtkorb@gmail.com>
2019-09-23 19:18:08 +0200

    Added code from week 5

---
+++ src/no/oslomet/cs/algdat/Week5.java
@@ -0,0 +1,80 @@
+package no.oslomet.cs.algdat;
+
+public class Week5 {
+
+    /**
+     * Merges values[begin, mid) with values[mid, end)
+     * @param values
+     * @param begin
+     * @param mid
+     * @param end
+     */
+    public static void merge(int[] values, int[] tmp, int begin, int mid, int end) {
+        //Already sorted
+        if (values[mid-1] <= values[mid]) {
+            return;
+        }
+
+        int n = mid - begin;
+
+        //Kopier fra values inn i temp
+        System.arraycopy(values, begin, tmp, 0, n);
+
+        //Int i teller i tmp-variabelen
+        int i = 0;
+
+        //j-telleren er i values, starter fra midten
+        int j = mid;
+
+        //k-telleren er i values - men brukes som output. Starer på begin
+        int k = begin;
+
+        //Så lenge i er innenfor intervallet til "a"
+        while (i < n && j < end) {
+            if (tmp[i] < values[j]) {
+                values[k] = tmp[i];
+                ++i;
+                ++k;
+            }
+            else {
+                values[k] = values[j];
+                ++j;
+                ++k;
+            }
+        }
+
+        //Legg inn resterende tall
+        while (i < n) {
+            values[k] = tmp[i];
+            ++i;
+            ++k;
+        }
+    }
+
+    public static void mergeSort(int[] values, int[] tmp, int begin, int end) {
+        int mid = (begin + end) / 2;
+
+        if (end - begin < 2) {
+            return;
+        }
+
+        //Sort left interval
+        mergeSort(values, tmp, begin, mid);
+
+        //Sort right interval
+        mergeSort(values, tmp, mid, end);
+
+        //Merges values into a single table
+        merge(values, tmp, begin, mid, end);
+    }
+
+    /**
+     * Starter rekursjonen
+     * @param values
+     */
+    public static void mergeSort(int[] values) {
+        int[] tmp = new int[values.length/2];
+
+        mergeSort(values, tmp, 0, values.length);
+    }
+}
---
+++ src/no/oslomet/cs/algdat/Week5Test.java
@@ -0,0 +1,34 @@
+package no.oslomet.cs.algdat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Week5Test {
+
+    @Test
+    void merge() {
+        int[] values = {0, 4, 5, 7, 9, 13,
+                        2, 3, 6, 8, 11, 12};
+        int[] tmp = new int[6];
+
+        Week5.merge(values, tmp, 0, 6, values.length);
+        System.out.println(Arrays.toString(values));
+
+        int[] sorted = {0, 2, 3, 4, 5, 6,
+                        7, 8, 9, 11, 12, 13};
+        assertArrayEquals(sorted, values);
+    }
+
+    @Test
+    void mergeSort() {
+        int values[] = {0, 9, 7, 6, 2, 1, 5, 3, 4};
+        int reference[] = {0, 1, 2, 3, 4, 5, 6, 7, 9};
+
+        Week5.mergeSort(values);
+
+        assertArrayEquals(reference, values);
+    }
+}
