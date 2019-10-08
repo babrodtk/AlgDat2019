aa41b236bce54e4150b5e5e672a7b944cf91f8ef
babrodtk <andre.r.brodtkorb@gmail.com>
2019-10-08 16:49:19 +0200

    Added code from this weeks lecture

---
+++ src/no/oslomet/cs/algdat/Week8.java
@@ -0,0 +1,54 @@
+package no.oslomet.cs.algdat;
+
+public class Week8 {
+
+    public static class CircularDeque {
+        int begin; //< Starten av køen
+        int end; //< slutten av køen
+        int capacity; //< maksimal størrelse på køen
+        int size; //< antall elementer i køen så langt
+        char[] values;
+
+        CircularDeque(int capacity) {
+            this.begin = 0;
+            this.end = 0;
+            this.capacity = capacity;
+            this.size = 0;
+            this.values = new char[capacity];
+        }
+
+        void pushBack(char value) {
+            //fraTilKontroll....
+            if (size == capacity) {
+                throw new ArrayIndexOutOfBoundsException("Køen er full!");
+            }
+
+            System.out.println("Legger til " + value + " på plass " + end + ", size=" + (size+1));
+
+            values[end] = value;
+            end = (end+1) % capacity;
+            size = size + 1;
+        }
+
+        char popFront() {
+            if (size == 0) {
+                throw new ArrayIndexOutOfBoundsException("Køen er tom!");
+            }
+
+            char retval = values[begin];
+
+            System.out.println("Henter ut " + retval + " på plass " + begin + ", size=" + (size-1));
+
+            begin = (begin+1) % capacity;
+            size = size - 1;
+
+            return retval;
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Hello week 8");
+    }
+
+
+}
---
+++ src/no/oslomet/cs/algdat/Week8Test.java
@@ -0,0 +1,44 @@
+package no.oslomet.cs.algdat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Week8Test {
+
+    @Test
+    void CircularDequeTest() {
+        Week8.CircularDeque c_deque = new Week8.CircularDeque(6);
+
+        c_deque.pushBack('A');
+        c_deque.pushBack('B');
+        c_deque.pushBack('C');
+        c_deque.pushBack('D');
+        c_deque.pushBack('E');
+        c_deque.pushBack('F');
+
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> c_deque.pushBack('G'));
+
+        assertEquals('A', c_deque.popFront());
+        assertEquals('B', c_deque.popFront());
+        assertEquals('C', c_deque.popFront());
+        assertEquals('D', c_deque.popFront());
+        assertEquals('E', c_deque.popFront());
+        assertEquals('F', c_deque.popFront());
+
+    }
+
+
+
+    @Test
+    void CircularDequeTest2() {
+        Week8.CircularDeque c_deque = new Week8.CircularDeque(6);
+
+        char[] my_chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+        for (int i=0; i<my_chars.length; ++i) {
+            c_deque.pushBack(my_chars[i]);
+            assertEquals(my_chars[i], c_deque.popFront());
+        }
+    }
+}
